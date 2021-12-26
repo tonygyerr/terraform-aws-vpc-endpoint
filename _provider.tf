69cb1c9e9862b56815b552959b02364e6b6f7a8b
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-12-26 13:12:50 -0500

    profile

--- _provider.tf
+++ _provider.tf
@@ -2,8 +2,3 @@ provider "aws" {
   region = var.aws_region
   profile = var.profile
 }
-
-# terraform {
-#   backend "s3" {
-#   }
-# }
--- examples/providers.tf
+++ examples/providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  version = "~> 2.0"
+  # version = "~> 2.0"
   region  = "us-east-1"
-  profile = "default"
+  profile = var.profile
 }
 
 terraform {
